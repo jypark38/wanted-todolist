af1b114a350f5cc6aa89880479b2df732996ef8f
jypark38 <jury124@naver.com>
2024-02-29 14:46:15 +0900

    컴포넌트 분리

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,44 +1,8 @@
 import React from "react";
 import styles from "./List.module.css";
-import {
-  completeTodo,
-  deleteTodo,
-  selectTodoById,
-} from "@/store/slice/todosSlice";
-import { EntityId } from "@reduxjs/toolkit";
-import { selectFilterTodoIds } from "../../store/slice/todosSlice";
-import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import Button from "../Button";
-
-interface ItemProps {
-  todoId: EntityId;
-}
-
-const Item = React.memo(({ todoId }: ItemProps) => {
-  const item = useAppSelector((state) => selectTodoById(state, todoId));
-
-  const dispatch = useAppDispatch();
-
-  const handleDelete = () => dispatch(deleteTodo(todoId));
-  const handleComplete = () =>
-    dispatch(completeTodo({ ...item, completed: !item.completed }));
-
-  const completedText = item.completed ? "취소" : "완료";
-  const itemStatus = item.completed ? "completed" : "pending";
-
-  return (
-    <article className={styles.Item}>
-      <div className={styles.TextContainer}>
-        <p className={styles.TodoText}>{item.text}</p>
-        <span className={styles.Status}>{itemStatus}</span>
-      </div>
-      <div className={styles.ButtonContainer}>
-        <Button option={completedText} handleClick={handleComplete} />
-        <Button option={"삭제"} handleClick={handleDelete} />
-      </div>
-    </article>
-  );
-});
+import { selectFilterTodoIds } from "@/store/slice/todosSlice";
+import { useAppSelector } from "@/store/hooks";
+import Item from "./Item";
 
 const List = () => {
   const filterTodosIds = useAppSelector(selectFilterTodoIds);
